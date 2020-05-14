2bf6c4595cf605cdd472792f4bf840394453ceae
James Dawson <james@Mac.default>
2020-05-14 17:13:48 +0100

    reuploaded current files

---
+++ palettes.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', () => {
+
+    window.palette = {};
+    window.palette.autumn = {
+        kobe: '#7D220E',
+        brownSugar: '#B56235',
+        darkSienna: '#250902',
+        richBlack: '#050200',
+        lavanderBlush: '#EADEE3'
+    };
+
+    window.palette.explosion = {
+        greenYellow: '#FEF2AA',
+        safetyOrange: '#FF7B12',
+        kobe: '#7D220E',
+        vanDykeBrown: '#6B3E26',
+        smokyBlack: '#1F0B01'
+    };
+
+});
